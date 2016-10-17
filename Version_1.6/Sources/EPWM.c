8c24cd4115791a823f09fab9c566846978e45e90
Anouar <updnv@student.kit.edu>
2016-10-17 19:39:56 +0200

    ADC, oc, ov,+ disable duty cycle  : ok (gezeigt)

---
+++ Version_1.6/Sources/EPWM.c
@@ -0,0 +1,400 @@
+/*
+ * EPWM.c
+ *
+ *  Created on: 06.07.2016
+ *      Author: Lanwer
+ */
+
+#include "MK22F51212.h"
+#include "mcu_tracer.h"
+
+
+int s=0;
+int c=0;
+//Set up protection functions
+//required write enable.
+void FTM_protect_init(){
+
+	//Clock to the comparator
+	SIM_SCGC4|=SIM_SCGC4_CMP_MASK;
+	SIM_SCGC5|=SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTA_MASK ; ////////////////////
+
+
+	// Init Overcurrent protection
+
+	// for the positive input of the CMP0, the IN1 is used (PTC7)
+	///////////////////////// THIS SHOULD NOT BE NECESSARY
+	PORTC_PCR7= PORT_PCR_MUX(0);
+
+	// Enable the comparator
+	CMP0_CR1=CMP_CR1_EN_MASK;
+
+	CMP0_CR0=CMP_CR0_FILTER_CNT(0);
+	// Enable DAC, VIn1, Voltage 19/60*Vref
+	// For the test, a max current of 1A is used
+	// ==> U_shunt_max = 0,1 x 1  = 0,1V
+	// the signal is amplified with the OP (8x)
+	// so the maximum voltage is 0,8V which is routed to the INP1 of CMP0
+	// the OR Function realized with the diodes reduces the voltage coming to INP1 pin by approximately 0,45 V
+	// So the maximum voltage to compare to the DAC Reference is : 0,8V - 0,45V = 0,35 V
+	// VIn1 = V_ref = 1.2113 V
+	// Voltage (19/64) *Vref = 0,3596 V
+
+	CMP0_DACCR=CMP_DACCR_DACEN(1)|CMP_DACCR_VRSEL(0)|CMP_DACCR_VOSEL(18);
+	CMP0_MUXCR=CMP_MUXCR_PSEL(1)|CMP_MUXCR_MSEL(7); //+=Sense -=DAC
+
+	// Init Overvoltage protection
+	// config port
+	// for the positive input of the CMP0, the IN1 is used (PTC3)
+	///////////////////////// THIS SHOULD NOT BE NECESSARY
+	PORTC_PCR3= PORT_PCR_MUX(0);
+
+	// Enable the comparator
+	CMP1_CR1=CMP_CR1_EN_MASK;
+	// Set filter to react after 7 consequent samples
+	CMP1_CR0=CMP_CR0_FILTER_CNT(0);
+
+	//CMP1_SCR= CMP_SCR_IER_MASK ;
+
+
+	//Overvoltage Level Calculation:
+	//(0.5+Value)*17V
+	//800V: 47
+
+	// Enable DAC, VIn1, Voltage 40/60*Vref
+	// For the test, a max output voltage of is 40V :
+	// Passing through the voltage divider it becomes:
+	// 40 x 7,5 / (1000 + 7,5) = 0,298 V
+	// the OR Function realized with the diodes reduces the voltage coming to INP1 pin by approximately 0,45 V
+	// So the maximum voltage to compare to the DAC Reference is : V = 0,35 V
+	// VIn1 = V_ref = 1.2113 V
+	// Voltage (19/64) *Vref = 0,3596 V
+
+	// For the test, a voltage of 0,8V is applied directly to the CMP1_IN1
+	// The OR function realized by the Schottky diodes is bypassed to avoid applying high voltages to the output during the tests
+	// VIn1 = V_ref = 1.2113 V
+	// Voltage (40/64) *Vref = 0,757 V
+	// So the applied voltage (0,8) should do the work !
+	CMP1_DACCR=CMP_DACCR_DACEN(1) | CMP_DACCR_VRSEL(0)|CMP_DACCR_VOSEL(3);
+
+
+	// select input source (CMP1_In1)
+	CMP1_MUXCR=CMP_MUXCR_PSEL(1)|CMP_MUXCR_MSEL(7); //+=Sense -=DAC
+
+
+
+
+	//Select comparator outputs (CMP0 out for OC Prot and CMP1 out for OV Prot) as fault source of FTM0
+	SIM_SOPT4|=(SIM_SOPT4_FTM0FLT1_MASK)|(SIM_SOPT4_FTM0FLT0_MASK);
+
+	// Fault control is enabled for all channels, and the selected mode is the manual fault clearing.
+	// write protected ==> to disable the protection: MODE[WPDIS] = 1
+	FTM0_MODE|= FTM_MODE_WPDIS_MASK | FTM_MODE_FAULTM(3);
+
+	// Enabling the fault input 0 and 1
+	FTM0_FLTCTRL=FTM_FLTCTRL_FAULT0EN_MASK|FTM_FLTCTRL_FAULT1EN_MASK;
+
+	// Enabling the fault control for all the channels  0 ==> 7
+	//FTM0->COMBINE|=FTM_COMBINE_FAULTEN0_MASK|FTM_COMBINE_FAULTEN1_MASK|FTM_COMBINE_FAULTEN2_MASK|FTM_COMBINE_FAULTEN3_MASK;
+
+	// Clearing the Fault Detection Flag
+	FTM0_FMS&=(~FTM_FMS_FAULTF_MASK);
+
+
+}
+
+void FTM_fault_interrupt_enable(){
+	//inits the fault interrupt function
+	FTM0_MODE|=FTM_MODE_FAULTIE_MASK;
+	NVIC_EnableIRQ(FTM0_IRQn);
+}
+
+void FTM0_IRQHandler(void){
+
+	FTM0_MODE&=~FTM_MODE_FAULTIE_MASK;
+	//CMP0 works on Overcurrent
+	if(FTM0_FMS & FTM_FMS_FAULTF0_MASK){
+		mcu_tracer_msg("Overcurrent detected. PWM off.");
+		GPIOA_PSOR |= (1<<5);
+		GPIOD_PSOR = (1<<3);
+        c=1;
+
+	}else if(FTM0_FMS & FTM_FMS_FAULTF1_MASK){
+		mcu_tracer_msg("Overvoltage detected. PWM off.");
+		GPIOA_PSOR |= (1<<13);
+		GPIOD_PSOR = (1<<3);
+		s=1 ;
+	}else{
+		mcu_tracer_msg("OverX: Unknown error. Fix me!");
+	}
+
+}
+
+/*
+void FTM_clear_error(void){
+	//Clears overcurrent error (follow description in FAULTF Field on page 920)
+	// reading the FMS register
+	uint32_t fms=FTM0_FMS;
+	// Check if there is a fault detected
+	if(!(fms & FTM_FMS_FAULTIN_MASK)){
+		// resetting the FAULTF bit in fms variable
+		fms&=(~FTM_FMS_FAULTF_MASK);
+		// copying the fms variable to FMS register
+		FTM0_FMS=fms;
+		//Enable Interrupts
+		FTM_fault_interrupt_enable();
+		mcu_tracer_msg("OverX: Errors reseted.");
+		//GPIOD_PCOR = (1<<3);
+
+		GPIOA_PCOR = (1<<5) | (1<<13);
+	}else{
+		mcu_tracer_msg("OverX: Error still persistend. Cannot reset.");
+	}
+}
+
+*/
+
+
+void FTM_clear_error(void){
+	//Clears overcurrent error
+	uint32_t fms=FTM0->FMS;
+	if(!(fms & FTM_FMS_FAULTIN_MASK)){
+		fms&=(~FTM_FMS_FAULTF_MASK);
+		FTM0->FMS=fms;
+		//Enable Interrupts
+		FTM_fault_interrupt_enable();
+		mcu_tracer_msg("OverX: Errors reseted.");
+	}else{
+		mcu_tracer_msg("OverX: Error still persistend. Cannot reset.");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+// Setting the duty cycle of the EPWM , a = 0..100   (%)
+void PWM_set_dutycycle (int a) {
+
+//Loading updated values is enabled.
+FTM0_PWMLOAD|= FTM_PWMLOAD_LDOK_MASK ;
+
+//  match value for the output modes for all the channels
+FTM0_C0V=(a*170)/100 ; //  match value for the output modes (CH0)
+FTM0_C1V=(a*170)/100 ; //  match value for the output modes (CH1)
+FTM0_C2V=(a*170)/100 ; //  match value for the output modes (CH2)
+FTM0_C3V=(a*170)/100 ; //  match value for the output modes (CH3)
+FTM0_C4V=(a*170)/100 ; //  match value for the output modes (CH4)
+FTM0_C5V=(a*170)/100 ; //  match value for the output modes (CH5)
+FTM0_C6V=(a*170)/100 ; //  match value for the output modes (CH6)
+FTM0_C7V=(a*170)/100 ; //  match value for the output modes (CH7)
+}
+
+void PWM_set_dutycycle1 (int a) {
+
+//Loading updated values is enabled.
+FTM0_PWMLOAD|= FTM_PWMLOAD_LDOK_MASK ;
+//  match value for the output modes ( CH4 and CH5 ) for the PWM signals of the first Buck converter
+FTM0_C4V=(a*170)/100 ; //  match value for the output modes (CH4)
+FTM0_C5V=(a*170)/100 ; //  match value for the output modes (CH5)
+
+}
+
+void PWM_set_dutycycle2 (int a) {
+
+//Loading updated values is enabled.
+FTM0_PWMLOAD|= FTM_PWMLOAD_LDOK_MASK ;
+//  match value for the output modes ( CH0 and CH1 ) for the PWM signals of the second Buck converter
+FTM0_C0V=(a*170)/100 ; //  match value for the output modes (CH0)
+FTM0_C1V=(a*170)/100 ; //  match value for the output modes (CH1)
+
+}
+
+void PWM_set_dutycycle3 (int a) {
+
+//Loading updated values is enabled.
+FTM0_PWMLOAD|= FTM_PWMLOAD_LDOK_MASK ;
+//  match value for the output modes ( CH0 and CH1 ) for the PWM signals of the third Buck converter
+FTM0_C2V=(a*170)/100 ; //  match value for the output modes (CH2)
+FTM0_C3V=(a*170)/100 ; //  match value for the output modes (CH3)
+
+}
+
+void PWM_set_dutycycle4 (int a) {
+
+//Loading updated values is enabled.
+FTM0_PWMLOAD|= FTM_PWMLOAD_LDOK_MASK ;
+//  match value for the output modes ( CH0 and CH1 ) for the PWM signals of the fourth Buck converter
+FTM0_C6V=(a*170)/100 ; //  match value for the output modes (CH6)
+FTM0_C7V=(a*170)/100 ; //  match value for the output modes (CH7)
+
+}
+
+
+// Enables the deadtime insertion for all the channels (n) and (n+1).
+void PWM_deadtime_enable (void) {
+	FTM0_COMBINE |= FTM_COMBINE_DTEN0_MASK | FTM_COMBINE_DTEN1_MASK |FTM_COMBINE_DTEN2_MASK |FTM_COMBINE_DTEN3_MASK   ;
+}
+
+
+
+// Divide the system clock by 1 (deadtime prescaler) and Deadtime value : Deadtime insert value = (DTPS × DTVAL).  (Maximaler wert = 63)
+void PWM_set_deadtime (int b, int c)  {                              // b: deadtime prescaler value b= 0..3  ;  c: Deadtime Value c= 0..63 ;
+FTM0_DEADTIME = FTM_DEADTIME_DTPS(b)| FTM_DEADTIME_DTVAL(c) ;
+}
+
+// Set the EPWM frequency throigh the MOD and CNTIN and FTM clock values.
+void PWM_set_frequency(void) {
+	// writing the modulo value for the FTM Counter ==> Frequency
+	FTM0_MOD=170;
+	//  setting the initial value for the FTM counter to 0
+	FTM0_CNTIN = 0x00;
+	// clock source selection : System clock and Prescaler Factor Selection : devide by 1
+	FTM0_SC = FTM_SC_CLKS(1) | FTM_SC_PS(0);
+}
+
+// Initialiase the Flextimer to work as an edge aligned PWM in complementary mode
+// (enabling the PORT clocks (SIM_SCGC5) and the FTM clock, Muxing the pins for the channels, enabling the FTM)
+void PWM_init(void) {
+
+	// The used Channel 0 ==> 7 are on port C and D ==> Enable the clock of PORT C and D (SIM_SCGC5)
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTD_MASK;
+	// Setting the used pins for FTM function
+	// FTM0 channel 0 (PTC1 ==> J24_6)
+	PORTC_PCR1 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 1 (PTC2 ==> J24_8)
+	PORTC_PCR2 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 2
+	PORTC_PCR5 = PORT_PCR_MUX(7) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 3
+	PORTC_PCR4 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 4
+	PORTD_PCR4 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 5
+	PORTD_PCR5 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 6
+	PORTD_PCR6 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+	// FTM0 channel 7
+	PORTD_PCR7 = PORT_PCR_MUX(4) | PORT_PCR_DSE_MASK;
+
+
+	// disable pin for all the gate drivers in case of over current or over voltage
+	PORTD_PCR3 = PORT_PCR_MUX(1) | PORT_PCR_DSE(0);
+	// alt 1 for PTA13 (yellow led) for overvoltage
+	PORTA_PCR13 = PORT_PCR_MUX(1) | PORT_PCR_DSE(0);
+	// alt 1 for PTA5 (green led)  for overcurrent
+	PORTA_PCR5 = PORT_PCR_MUX(1) | PORT_PCR_DSE(0);
+
+    // Set direction : output
+	// disable pin
+	GPIOD_PDDR|=(1<<3);
+    // overvoltage led output (yellow) PTA13
+	GPIOA_PDDR|=(1<<13);
+    // overcurrent led output (green)  PTA5
+	GPIOA_PDDR|=(1<<5);
+	/*
+	// The used Channels 0 ==> 7 are on port C and D ==> Enable the clock of PORT C and D (SIM_SCGC5)
+		SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTD_MASK;
+		// Setting the used pins for FTM function
+
+		// FTM0 channel 0
+		PORTC_PCR1 = PORT_PCR_MUX(4);
+		// FTM0 channel 1
+		PORTC_PCR2 = PORT_PCR_MUX(4) ;
+		// FTM0 channel 2
+		PORTC_PCR5 = PORT_PCR_MUX(7) ;
+		// FTM0 channel 3
+		PORTC_PCR4 = PORT_PCR_MUX(4) ;
+		// FTM0 channel 4
+		PORTD_PCR4 = PORT_PCR_MUX(4) ;
+		// FTM0 channel 5
+		PORTD_PCR5 = PORT_PCR_MUX(4) ;
+		// FTM0 channel 6
+		PORTD_PCR6 = PORT_PCR_MUX(4) ;
+		// FTM0 channel 7
+		PORTD_PCR7 = PORT_PCR_MUX(4) ;
+	*/
+
+
+	//  enable FTM0 and FTM0 module clock
+	SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK ;
+	//  Write protection is disabled. Write protected bits can now be written.
+	FTM0_FMS &= ~FTM_FMS_WPEN_MASK ;
+	// FTM enable and Write protection disabled , to be able to write in FTMx_CnSC and FTMx_SC and other registers
+	FTM0_MODE |= FTM_MODE_WPDIS_MASK | FTM_MODE_FTMEN_MASK ;
+
+
+	// FTM Protection function
+	FTM_protect_init();
+
+
+	// Edge Aligned PWM low-true pulses (set Output on match)
+	FTM0_C0SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C1SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C2SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C3SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C4SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C5SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C6SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+	FTM0_C7SC= (FTM_CnSC_MSB(1) | FTM_CnSC_MSA(0) | FTM_CnSC_ELSB(0) | FTM_CnSC_ELSA(1) );
+
+	// Enables Complementary mode and the PWM synchronization for the combined channels.
+	//In Complementary mode the channel (n+1) output is the inverse of the channel (n) output.
+	FTM0_COMBINE |= FTM_COMBINE_COMP0_MASK | FTM_COMBINE_COMP1_MASK |FTM_COMBINE_COMP2_MASK |FTM_COMBINE_COMP3_MASK |
+			        FTM_COMBINE_SYNCEN0_MASK| FTM_COMBINE_SYNCEN1_MASK| FTM_COMBINE_SYNCEN2_MASK| FTM_COMBINE_SYNCEN3_MASK |
+	                FTM_COMBINE_FAULTEN0_MASK | FTM_COMBINE_FAULTEN1_MASK | FTM_COMBINE_FAULTEN2_MASK | FTM_COMBINE_FAULTEN3_MASK;
+
+
+// --------------------------------------------------------------------------------------------------//
+	// Deadtime configuration:
+	// Enables the deadtime insertion in the channels (n) and (n+1).
+	PWM_deadtime_enable() ;
+
+	// Deadtime calculation [s]= (DTPS × DTVAL) / (60 Mhz)
+	// a: deadtime prescaler value DTPS:  b= 0..3 ( see p. 914 for the prescaler values); 0 or 1 means devide by 1; 2 means devide by 4 ; 3 means devide by 16 ;
+	// b: Deadtime Value c= 0..63 ;
+	// Example : for DTPS = 2  (means 4) and DTVAL = 3 ==> Deadtime [s] = (4 * 3) / 60 Mhz = 200 ns
+	PWM_set_deadtime (2,2) ;
+
+// --------------------------------------------------------------------------------------------------//
+	// Set the EPWM frequency throigh the MOD and CNTIN and FTM clock values.
+	// PWM Frequency = FTM Frequency / ( MOD - CNTIN +1 ) = 60 Mhz / ( 170 - 0 + 1 ) = 350.87 Khz
+	PWM_set_frequency() ;
+
+
+// --------------------------------------------------------------------------------------------------//
+	// PWM Synchronization: (synchronization was already enabled in FTMx_CMOBINE register)
+    // to enable the update of FTM Registers (such as CnV register to set the duty cycle), we need to configure the PWM synchronization
+	// for that, I followed the flowchart in AN4560 (Page 5)
+	/* When the SWSYNC bit (Software synch) is set to 1, the duty cycle update synchronization is controlled by CNTMAX and CNTMIN. These bits define where
+       the duty cycle update takes place. If CNTMAX is set to 1, the duty cycle update is done when the FTM
+	   counter reaches its maximum value. If CNTMIN is set to 1, the duty cycle update is done when the FTM
+	   counter reaches its minimum value. The software duty cycle update is typically for PMSM motor control applications.
+	   So it should be also fine to work with it for the buck converter */
+	FTM0_SYNC|= FTM_SYNC_CNTMIN_MASK  | FTM_SYNC_SWSYNC_MASK ;
+
+	// The enhanced PWM synchronization mode is recommended for motor control and power conversion applications ==> SYNCONF[SYNCMODE] = 1
+	/* If the SWRSTCNT bit is set, the FTM counter restarts with FTM_CNTIN register value and the FTM_MOD and FTM_CnV registers are updated immediately.
+	   If the SWRSTCNT bit is cleared, the FTM counter continues to count normally and the FTM_MOD and FTM_CnV register update at the next loading point. */
+	// SWWRBUF The software trigger activates MOD, CNTIN, and CV registers synchronization.
+	FTM0_SYNCONF|= FTM_SYNCONF_SYNCMODE_MASK |  FTM_SYNCONF_SWWRBUF_MASK | FTM_SYNCONF_SWRSTCNT_MASK ;
+
+	// PWM Synchronization mode : no restrictions
+	 FTM0_MODE &= ~FTM_MODE_PWMSYNC_MASK ;
+
+	 FTM_fault_interrupt_enable();
+
+
+	 /////////////////
+	 //FTM_clear_error();
+
+
+}
+
+
---
+++ Version_1.6/Sources/adc.c
@@ -0,0 +1,182 @@
+
+#include "MK22F51212.h"
+#include "VREF.h"
+#include "adc.h"
+
+//The calibration function sets the offset calibration value, the minus-side calibration values, and the plus-side calibration values.
+/* For best calibration results: (See page 795 of the reference manual)
+• Set hardware averaging to maximum, that is, SC3[AVGE]=1 and SC3[AVGS]=11 for an average of 32
+• Set ADC clock frequency fADCK less than or equal to 4 MHz
+• VREFH=VDDA
+• Calibrate at nominal voltage and temperature
+return 1 if failed ==> ADC accuracy specifications are not guaranteed, otherweise return 0 */
+int ADC_calibrate(void)
+{
+
+	// Calibrating ADC0
+	// Set ADC clock frequency fADCK less than or equal to 4 MHz ==> input clock is the ALTCLK OSCERCLK (see page 105) [8 Mhz]
+	// Divide the clock by 4 ==>  fADK = 2Mhz (this clock is just used for the calibration process)
+	// choose the 16-bits mode
+	ADC0_CFG1 |= ADC_CFG1_ADICLK(2)| ADC_CFG1_ADIV(2) | ADC_CFG1_MODE(3);
+	// Set hardware averaging to maximum, that is, SC3[AVGE]=1 and SC3[AVGS]=11 (3) for an average of 32
+	// and then start the calibration
+	ADC0_SC3 |= ADC_SC3_AVGS(3) | ADC_SC3_AVGE_MASK | ADC_SC3_CAL_MASK;
+	// Wait for the calibration to be done
+	while(ADC0_SC3 & ADC_SC3_CAL_MASK);
+	// Check if any error occured while calibrating and return 1 if failed ==> ADC accuracy specifications are not guaranteed, otherweise return 0
+    if(ADC0_SC3 & ADC_SC3_CALF_MASK)
+		    return 1;
+
+	// follow the steps on page 796:
+
+    // Initialize or clear a 16-bit variable in RAM.
+    uint16_t variable = 0;
+    // Add the plus-side calibration results CLP0, CLP1, CLP2, CLP3, CLP4, and CLPS to the variable.
+    variable += ADC0_CLPS +  ADC0_CLP0 + ADC0_CLP1 + ADC0_CLP2 + ADC0_CLP3 + ADC0_CLP4;
+    // Divide the variable by two.
+    variable /= 2;
+    // Set the MSB of the variable.
+    variable |= 0b1000000000000000;
+	// Store the value in the plus-side gain calibration register PG.
+    ADC0_PG = variable;
+	// Repeat the procedure for the minus-side gain calibration value.
+	variable = 0;
+	variable += ADC0_CLMS + ADC0_CLM0 + ADC0_CLM1 + ADC0_CLM2 + ADC0_CLM3 + ADC0_CLM4;
+	variable /= 2;
+	variable |= 0b1000000000000000;
+	ADC0_MG = variable;
+
+
+	// Calibrating ADC1
+	// Set ADC clock frequency fADCK less than or equal to 4 MHz ==> input clock is the ALTCLK OSCERCLK (see page 105) [8 Mhz]
+	// Devide the clock by 4 ==>  fADK = 2Mhz (this clock is just used for the calibration process)
+	// choose the 16-bits mode
+	ADC1_CFG1 |= ADC_CFG1_ADICLK(2)| ADC_CFG1_ADIV(2) | ADC_CFG1_MODE(3);
+	// Set hardware averaging to maximum, that is, SC3[AVGE]=1 and SC3[AVGS]=11 (3) for an average of 32
+	// and then start the calibration
+	ADC1_SC3 |= ADC_SC3_AVGS(3) | ADC_SC3_AVGE_MASK | ADC_SC3_CAL_MASK;
+	// Wait for the calibration to be done
+	while(ADC1_SC3 & ADC_SC3_CAL_MASK);
+	// Check if any error occured while calibrating and return 1 if failed ==> ADC accuracy specifications are not guaranteed, otherweise return 0
+    if(ADC1_SC3 & ADC_SC3_CALF_MASK)
+		    return 1;
+	// follow the steps on page 796:
+
+    // Initialize or clear a 16-bit variable in RAM.
+    variable = 0;
+    // Add the plus-side calibration results CLP0, CLP1, CLP2, CLP3, CLP4, and CLPS to the variable.
+    variable += ADC1_CLPS +  ADC1_CLP0 + ADC1_CLP1 + ADC1_CLP2 + ADC1_CLP3 + ADC1_CLP4;
+    // Divide the variable by two.
+    variable /= 2;
+    // Set the MSB of the variable.
+    variable |= 0b1000000000000000;
+	// Store the value in the plus-side gain calibration register PG.
+    ADC1_PG = variable;
+	// Repeat the procedure for the minus-side gain calibration value.
+	variable = 0;
+	variable += ADC1_CLMS + ADC1_CLM0 + ADC1_CLM1 + ADC1_CLM2 + ADC1_CLM3 + ADC1_CLM4;
+	variable /= 2;
+	variable |= 0b1000000000000000;
+	ADC1_MG = variable;
+
+	       return 0;
+}
+
+
+/* ADC_init()
+ * Calibrates and initializes ADC0 and ADC1 to perform conversions from different channels
+ * and generate DMA requests at the end of the conversion, triggering the DMA channel to transfer the ADC result to SRAM
+ * ADC1 is used for current values and ADC0 for voltage values
+ */
+void ADC_init(void)
+{
+	// Configure a voltage reference to get a VREF = 1.209 V
+	//VREF_Init();
+
+	// Enable the necessary clocks : 1) for the ADC0 and ADC1 Modules ; 2) for the PORTB and PORTC
+	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK | SIM_SCGC6_ADC1_MASK;	// ADC0 and ADC1 clock
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTC_MASK;	// PTB clock and PORT C
+
+	// pin muxing for the ADC0 channels:
+	// pin muxing for pin number 35 (PTB0/LLWU_P5); choose the alternative 0 : ADC0_SE8
+	PORTB_PCR0 |= PORT_PCR_MUX(0) | PORT_PCR_DSE_MASK;
+	// pin muxing for pin number 36 (PTB1); choose the alternative 0 : ADC0_SE9
+	PORTB_PCR1 |= PORT_PCR_MUX(0) | PORT_PCR_DSE_MASK;
+	// pin muxing for pin number 37 (PTB2); choose the alternative 0 : ADC0_SE12
+	PORTB_PCR2 |= PORT_PCR_MUX(0) | PORT_PCR_DSE_MASK;
+	// pin muxing for pin number 38 (PTB3); choose the alternative 0 : ADC0_SE13
+	PORTB_PCR3 |= PORT_PCR_MUX(0) | PORT_PCR_DSE_MASK;
+    // pin 18 for ADC0_SE23 is not on ports
+
+	// pin muxing for the ADC1 channels
+	// pin muxing for pin number 53 (PTC8) ; choose the alternative 0 : ADC0_SE4b
+	PORTC_PCR8  = PORT_PCR_MUX(0);
+	// pin muxing for pin number 54 (PTC9) ; choose the alternative 0 : ADC0_SE5b
+	PORTC_PCR9  = PORT_PCR_MUX(0);
+	// pin muxing for pin number 55 (PTC10); choose the alternative 0 : ADC0_SE6b
+	PORTC_PCR10 = PORT_PCR_MUX(0);
+	// pin muxing for pin number 56 (PTC11); choose the alternative 0 : ADC0_SE7b
+	PORTC_PCR11 = PORT_PCR_MUX(0);
+
+
+	// Calibrate the ADCs
+	ADC_calibrate();
+	ADC_calibrate();
+	ADC_calibrate();
+
+
+
+    //  1.2 V VREF_OUT is connected as the VALT reference option
+	ADC0_SC2 |= ADC_SC2_REFSEL(1) ;
+	ADC1_SC2 |= ADC_SC2_REFSEL(1) ;
+
+
+	// Reset the CFG1- register
+	ADC0_CFG1 = 0;
+	ADC1_CFG1 = 0;
+
+	// Set ADC clock frequency fADCK as the bus Clock (60 Mhz)
+	// Devide the clock by 8
+	// configure the module for 16-bit resolution
+	/* Sampling time configuration : sample time is the time that the internal switch is closed and sample capacitor is charged.
+	Long sample Time has been chosen for better results: This allows higher impedance inputs to be accurately sampled or to maximize
+	conversion speed for lower impedance inputs. Longer sample times can also be used to lower overall
+	power consumption when continuous conversions are enabled if high conversion rates are not required. */
+	ADC0_CFG1 |=  ADC_CFG1_ADICLK(0)| ADC_CFG1_ADIV(2) | ADC_CFG1_MODE(1) | ADC_CFG1_ADLSMP(1) ;
+	ADC1_CFG1 |=  ADC_CFG1_ADICLK(0)| ADC_CFG1_ADIV(3) | ADC_CFG1_MODE(3) | ADC_CFG1_ADLSMP(1) ;
+	// DMA is enabled and will assert the ADC DMA request during an ADC conversion complete event noted
+	// when any of the SC1n[COCO] flags is asserted
+	// ADC Software trigger mode: means that the ADC conversions are started by writing the channel number in the ADCx_SC1A register
+	ADC0_SC2 |= ADC_SC2_DMAEN_MASK | ADC_SC2_ADTRG(0);
+	ADC1_SC2 |= ADC_SC2_DMAEN_MASK | ADC_SC2_ADTRG(0);
+
+	// Default longest sample time; 20 extra ADCK cycles; 24 ADCK cycles total ==> this results a very stable state compared to short sample time
+	// High-speed conversion sequence selected with 2 additional ADCK cycles to total conversion time.
+	// for the ADC1, the channels ADxxb are used
+	ADC0_CFG2 = ADC_CFG2_ADLSTS(0) | ADC_CFG2_ADHSC_MASK ;
+	//ADC0_CFG2 = ADC_CFG2_ADLSTS(2)  ;
+	ADC1_CFG2 = ADC_CFG2_ADLSTS(0) | ADC_CFG2_ADHSC_MASK | ADC_CFG2_MUXSEL(1) ;
+
+	// SC1A is used for both software and hardware trigger modes of operation.
+	// Disable the module till the first read function == writing the number 31 in ADCH
+	ADC0_SC1A = ADC_SC1_ADCH(31);
+	ADC1_SC1A = ADC_SC1_ADCH(31);
+	// Start the conversion of Channel 8 to start the DMA cycle
+	ADC0_SC1A = ADC_SC1_ADCH(8);
+	ADC1_SC1A = ADC_SC1_ADCH(4);
+}
+
+
+
+/* This function reads the channel given in the parantheses and returns the result saved in the ADCx_RA register
+   The channel number is listed under ADCH in the ADCx_SC1n registerFor our case we used the channel ADC0_SE8  */
+unsigned short ADC_read(unsigned char channel)
+{   // writes to SC1A subsequently initiate a new conversion, if SC1[ADCH] contains a value other than all 1s. (other than disabled)
+	ADC0_SC1A = (channel & ADC_SC1_ADCH_MASK) ;
+	// Conversion in progress. wait for it to finish
+	while(ADC0_SC2 & ADC_SC2_ADACT_MASK);
+	// wait for the conversion to complete
+	while(!(ADC0_SC1A & ADC_SC1_COCO_MASK));
+	return ADC0_RA;
+}
+
