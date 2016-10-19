42449d6a788f8b62597af4554e410bd73d36f85b
Anouar <updnv@student.kit.edu>
2016-10-19 14:42:02 +0200

    good Regulator coefficients found

---
+++ Version_2.2.1/Includes/VREF.h
@@ -0,0 +1,15 @@
+/*
+ * VREF.h
+ *
+ *  Created on: 13.07.2016
+ *      Author: Lanwer
+ */
+
+#ifndef INCLUDES_VREF_H_
+#define INCLUDES_VREF_H_
+
+
+
+void VREF_Init(void);
+
+#endif /* INCLUDES_VREF_H_ */
